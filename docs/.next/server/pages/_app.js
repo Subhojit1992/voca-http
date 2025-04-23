d17579f1fbb3a4d81547f3961bbbf1f87881edae
subhojit1992 <subhojit1992.mondal@gmail.com>
2025-04-23 17:49:08 +0530

    changes

--- docs/.next/server/pages/_app.js
+++ docs/.next/server/pages/_app.js
@@ -5,47 +5,90 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 5656:
+/***/ 4065:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(968);
-/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_2__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(968);
+var head_default = /*#__PURE__*/__webpack_require__.n(head_);
+;// CONCATENATED MODULE: external "next/router"
+const router_namespaceObject = require("next/router");
+var router_default = /*#__PURE__*/__webpack_require__.n(router_namespaceObject);
+;// CONCATENATED MODULE: external "react-ga"
+const external_react_ga_namespaceObject = require("react-ga");
+var external_react_ga_default = /*#__PURE__*/__webpack_require__.n(external_react_ga_namespaceObject);
+;// CONCATENATED MODULE: ./pages/_app.tsx
+// @ts-nocheck 
 
 
 
+
+
+const TRACKING_ID = "G-8B0K62HERK";
 function MyApp({ Component , pageProps  }) {
-    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+    (0,external_react_.useEffect)(()=>{
+        // Initialize Google Analytics
+        external_react_ga_default().initialize(TRACKING_ID);
+        // Track page views
+        const handleRouteChange = (url)=>{
+            external_react_ga_default().pageview(url);
+        };
+        router_default().events.on("routeChangeComplete", handleRouteChange);
+        // Clean up event listener on unmount
+        return ()=>{
+            router_default().events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, []);
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
         children: [
-            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_2___default()), {
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
                 children: [
-                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
                         name: "viewport",
                         content: "width=device-width, initial-scale=1.0"
                     }),
-                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("title", {
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
                         children: "voca-http - Modern HTTP client for web applications"
                     }),
-                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
                         name: "description",
                         content: "A lightweight, flexible HTTP client for modern web applications"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("script", {
+                        async: true,
+                        src: `https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("script", {
+                        dangerouslySetInnerHTML: {
+                            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${TRACKING_ID}');
+            `
+                        }
                     })
                 ]
             }),
-            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+            /*#__PURE__*/ jsx_runtime_.jsx(Component, {
                 ...pageProps
             })
         ]
     });
 }
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
+/* harmony default export */ const _app = (MyApp);
 
 
 /***/ }),
@@ -78,7 +121,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(5656));
+var __webpack_exports__ = (__webpack_exec__(4065));
 module.exports = __webpack_exports__;
 
 })();
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+// @ts-nocheck 
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-8B0K62HERK';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize(TRACKING_ID);
+
+    // Track page views
+    const handleRouteChange = (url: string) => {
+      ReactGA.pageview(url);
+    };
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Clean up event listener on unmount
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>voca-http - Modern HTTP client for web applications</title>
         <meta name="description" content="A lightweight, flexible HTTP client for modern web applications" />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${TRACKING_ID}');
+            `,
+          }}
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
 
-export default MyApp; 
+export default MyApp;
